f1afb69bb20b717b58b1b4336998c255769ba89b
Jp Mendoza <jeanpauljr.mendoza@gmail.com>
2023-07-18 21:14:39 +0800

    Made a working projectile that curves

--- Assets/SlerpPractice/_Scripts/SlerpScript.cs
+++ Assets/SlerpPractice/_Scripts/SlerpScript.cs
@@ -10,83 +10,68 @@ namespace SlerpPractice
     {
 
 
-        [SerializeField] private Vector3 defaultPos;
-        [SerializeField] private Vector3 targetPos;
+        [SerializeField] private Vector2 defaultPos;
+        [SerializeField] private Vector2 targetPos;
         [SerializeField] private float moveAmount;
 
         [SerializeField] private Camera mainCamera;
         [SerializeField] private Vector3 mousePos;
 
-        [SerializeField] private float distanceCutoff;
-        [SerializeField] private float percentageOfTheDistance;
-        [SerializeField] private float trajectoryvalueX = 0.5f;
-        [SerializeField] private float distanceBetweenTarget;
-        [SerializeField] private int move;
-        [SerializeField] private float speed;
+        [SerializeField] private int shoot = 0;
+        [SerializeField] private bool overhand;
 
-        private void Awake()
+        private void Start()
         {
-            mainCamera = Camera.main.GetComponent<Camera>();
-        }
-        void Start()
-        {
+            transform.position = defaultPos;
         }
 
-        void Update()
+
+        private void Update()
         {
-            if (Input.GetButtonUp("Fire1"))
+            if(Input.GetButtonUp("Fire1"))
             {
-                speed = 0;
-                move = 0;
-                moveAmount = 0;
-                transform.position = defaultPos;
-
-                transform.position = defaultPos;
-                move = move == 1 ? 0 : 1;
+                shoot = shoot == 0 ? 1 : 0;
                 getTrajectory();
-                speed = getSpeed();
-            }
-            if (move != 0 && moveAmount < 1)
-            {
-                moveAmount += Time.deltaTime + (speed * 0.001f);
-                transform.position = Vector3.Slerp(defaultPos, targetPos, moveAmount);
-
             }
 
             if (Input.GetButtonUp("Fire2"))
             {
+                overhand = !overhand;
+            }
 
+            if (shoot == 1 && moveAmount < 1)
+            {
+                moveAmount += Time.deltaTime * 2;
+                transform.position = MathParabola.Parabola(defaultPos, targetPos, getHeight(overhand), moveAmount);
+            }
+
+            if (moveAmount >= 1)
+            {
+                shoot = 0;
+                moveAmount %= 1;
+                transform.position = defaultPos;
             }
 
 
-            //if (moveAmount > 1)
-            //{
-
-            //}
-
-
         }
-
 
         private void getTrajectory()
         {
             getMousePos();
-            getSpeed();
-            targetPos = new Vector3(mousePos.x, mousePos.y, trajectoryvalueX);
+            
+            targetPos = new Vector2(mousePos.x, mousePos.y);
         }
-
-        private void getMousePos ()
+        private void getMousePos()
         {
             mousePos = mainCamera.ScreenToWorldPoint(Input.mousePosition);
-
         }
 
-        private float getSpeed()
+        private float getHeight(bool isOverhand)
         {
-            distanceBetweenTarget = Vector2.Distance(defaultPos, targetPos);
-            float multiplier = Mathf.Lerp(distanceCutoff, 0f, distanceBetweenTarget/percentageOfTheDistance);
 
-            return multiplier;
+            float distance = Vector2.Distance(defaultPos, targetPos) / (isOverhand ? 4 : -4);
+
+            return distance;
         }
     }
 
